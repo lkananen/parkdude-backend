e2d99597429753626c18d4f5526d55c4e079942a
Sami Aalto <sami.aalto@tuni.fi>
2019-10-13 12:59:07 +0300

    Add validation rules

--- app/app.ts
+++ app/app.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 import {createRouter} from './router';
 import {StatusError} from './utils/errors';
 import {Request, Response, NextFunction, Express} from 'express';
-import {createConnection} from 'typeorm';
+import {ValidationError} from 'class-validator';
 
 export async function createApp(): Promise<Express> {
   await createConnection();
@@ -29,8 +29,23 @@ export async function createApp(): Promise<Express> {
       res.status(error.statusCode).json({message: error.message});
       return;
     }
+    if (Array.isArray(error) && error[0] instanceof ValidationError) {
+      // Error comes from class-validator
+      const errorMessages = flatten(
+        error.map((validationError: ValidationError) => Object.values(validationError.constraints))
+      );
+      res.status(400).json({
+        message: 'Validation failed:\n' + errorMessages.join('\n'),
+        errorMessages
+      });
+      return;
+    }
     res.status(500).json({message: 'Internal server error'});
   });
 
   return app;
+}
+
+function flatten<T>(arrays: T[][]): T[] {
+  return ([] as T[]).concat(...arrays);
 }
--- app/entities/parking-spot.ts
+++ app/entities/parking-spot.ts
@@ -1,4 +1,9 @@
-import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, CreateDateColumn, UpdateDateColumn} from 'typeorm';
+import {Length, validateOrReject} from 'class-validator';
+import {
+  BaseEntity, BeforeInsert, BeforeUpdate, Column,
+  CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn
+} from 'typeorm';
+
 
 @Entity()
 export class ParkingSpot extends BaseEntity {
@@ -6,6 +11,12 @@ export class ParkingSpot extends BaseEntity {
   id: string;
 
   @Column('varchar', {length: 200})
+  @Length(1, 200, {message: ({value, constraints}) => {
+    if (!value) {
+      return 'Name is required.';
+    }
+    return `Name ${value} is too long (${value && value.length} characters). Maximum length is ${constraints[1]}.`;
+  }})
   name: string;
 
   @CreateDateColumn()
@@ -13,4 +24,11 @@ export class ParkingSpot extends BaseEntity {
 
   @UpdateDateColumn()
   updated: Date;
+
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async validate() {
+    await validateOrReject(this);
+  }
 }
