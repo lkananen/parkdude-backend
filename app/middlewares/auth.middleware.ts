91b07e7d6dc3632ba9fde698466118c3c6440aa4
Sami Aalto <sami.aalto@tuni.fi>
2019-10-23 22:27:13 +0300

    Add basic authentication middleware

---
+++ app/middlewares/auth.middleware.ts
@@ -0,0 +1,26 @@
+import {Request, Response, NextFunction} from 'express';
+import {UserRole, User} from '../entities/user';
+
+
+export function loginRequired(req: Request, res: Response, next: NextFunction) {
+  const user = req.user as User;
+  if (user && [UserRole.VERIFIED, UserRole.ADMIN, UserRole.SLACK].includes(user.role)) {
+    next();
+  } else {
+    const status = user ? 401 : 403;
+    res.status(status).json({
+      message: 'Verified account required.'
+    });
+  }
+}
+
+export function adminRoleRequired(req: Request, res: Response, next: NextFunction) {
+  const user = req.user as User;
+  if (user && user.role === UserRole.ADMIN) {
+    next();
+  } else {
+    res.status(403).json({
+      message: 'Unauthorized: You do not have permission for this action.'
+    });
+  }
+}
