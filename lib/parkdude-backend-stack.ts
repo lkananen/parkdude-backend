e232e904931ca93c192ee52d84ff447eb64898a0
Lauri Kananen <lauri.kananen@solita.fi>
2019-12-05 12:37:51 +0200

    PostgreSQL database resources created to AWS stack.

--- lib/parkdude-backend-stack.ts
+++ lib/parkdude-backend-stack.ts
@@ -4,12 +4,15 @@ import lambda = require('@aws-cdk/aws-lambda');
 import dotenv = require('dotenv');
 import fs = require('fs');
 import path = require('path');
+import ec2 = require('@aws-cdk/aws-ec2');
+import rds = require('@aws-cdk/aws-rds');
 import {LambdaIntegration} from '@aws-cdk/aws-apigateway';
 import {Duration} from '@aws-cdk/core';
+import { SubnetType } from '@aws-cdk/aws-ec2';
 
 export class ParkdudeBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props);    
 
     const restApiHandler = new lambda.Function(this, 'RestApiHandler', {
       runtime: lambda.Runtime.NODEJS_10_X,
@@ -20,7 +23,7 @@ export class ParkdudeBackendStack extends cdk.Stack {
     });
 
     const restApi = new apigateway.RestApi(this, 'rest-api', {
-      restApiName: 'REST API',
+      restApiName: 'Parkdude REST API',
       description: 'This service serves widgets.'
     });
 
@@ -30,7 +33,56 @@ export class ParkdudeBackendStack extends cdk.Stack {
       anyMethod: true
     });
 
-    // TODO: More configurations (e.g. for production)
+    const parkdudeVpc = new ec2.Vpc(this, 'ParkdudeVPC', {
+      cidr: "10.0.0.0/24",       // Total of 2^(32-N) ip addresses in range. E.g. 2^6 = 256.
+      maxAzs: 2,                 // RDS instance requires at least 2
+
+      subnetConfiguration: [
+        {
+          subnetType: ec2.SubnetType.PUBLIC,
+          name: 'Parkdude-Ingress'
+        },
+        {
+          subnetType: ec2.SubnetType.PRIVATE,
+          cidrMask: 28,
+          name: 'Parkdude-Database'
+        }
+      ]
+    });
+
+    const parkdudeVPCSecGroup = new ec2.SecurityGroup(this, 'ParkdudeVPCSecGroup', {
+      vpc: parkdudeVpc,
+      allowAllOutbound: false
+    });
+
+    parkdudeVPCSecGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(5432),
+      'PostgreSQL default inbound port'
+    );
+
+    parkdudeVPCSecGroup.addEgressRule(
+      ec2.Peer.ipv4("10.0.0.0/24"),         // CIDR block for local VPC traffic
+      ec2.Port.tcp(5432),
+      'PostgreSQL default outbound port'
+    );
+
+    // Note! RDS has deletion protection on by default. This means that deleting the
+    // related CloudFormation stack doesn't delete the RDS, but instead makes it orphan.
+    // This is on to protect user data. Deletions should be made manually.
+    const instance = new rds.DatabaseInstance(this, 'ParkdudeDBInstance', {
+      engine: rds.DatabaseInstanceEngine.POSTGRES,
+      instanceClass: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
+      masterUsername: 'syscdk',         // By default the generated password is stored to secrets manager
+      vpc: parkdudeVpc,
+      vpcPlacement: parkdudeVpc.selectSubnets({
+        // Defines the public accessability of the DB
+        subnetType: SubnetType.PRIVATE
+      }),
+      securityGroups: [parkdudeVPCSecGroup],
+      databaseName: "ParkdudePSQL"
+    });
+
   }
 
   private getLambdaEnvironmentVariables(): dotenv.DotenvParseOutput {
@@ -38,3 +90,4 @@ export class ParkdudeBackendStack extends cdk.Stack {
     return dotenv.parse(fs.readFileSync(path.join(__dirname, '../env/app.sam-dev.env')));
   }
 }
+
