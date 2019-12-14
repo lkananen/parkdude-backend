a5cb7ebb9ba2235531e72b8d38bc2494c946ee6a
Janne Tommola <janne.tommola@gmail.com>
2019-12-14 15:34:19 +0200

    Partial implementation for session clearing

--- app/services/user.service.ts
+++ app/services/user.service.ts
@@ -1,5 +1,7 @@
 import {User, UserRole} from '../entities/user';
 import {UserBody, UserUpdateBody, UserData} from '../interfaces/user.interfaces';
+import {Session} from '../entities/session';
+import {getConnection} from 'typeorm';
 
 export async function fetchUsers(userRole?: UserRole): Promise<User[]> {
   if (!userRole) {
@@ -37,4 +39,12 @@ export async function updateUser(user: User, data: UserUpdateBody): Promise<User
 
 export async function deleteUser(user: User) {
   await user.remove();
+}
+
+export async function clearSession(user: User) {
+  const sessionRepo = getConnection().getRepository(Session);
+  // const sessions = await sessionRepo.find({where: {userId: user.id}});
+  const sessions = await sessionRepo.find();
+  console.log(sessions);
+  // await sessionRepo.remove(sessions);
 }
