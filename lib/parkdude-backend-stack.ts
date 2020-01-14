a8ee51bed82561ad80597ac1539bceb9515aeb19
Sami Aalto <sami.aalto@tuni.fi>
2020-01-15 01:25:26 +0200

    Add missing import

--- lib/parkdude-backend-stack.ts
+++ lib/parkdude-backend-stack.ts
@@ -12,6 +12,7 @@ import {LambdaIntegration} from '@aws-cdk/aws-apigateway';
 import {Duration} from '@aws-cdk/core';
 import {SubnetType} from '@aws-cdk/aws-ec2';
 import {Secret} from '@aws-cdk/aws-secretsmanager';
+import {Bucket} from '@aws-cdk/aws-s3';
 
 export class ParkdudeBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
